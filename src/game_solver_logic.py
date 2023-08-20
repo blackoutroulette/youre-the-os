cfe9c1abffab37a0f967a0fdf4020354288e0603
BlackoutRoulette <blackoutroulette@live.de>
2023-08-20 22:10:26 +0200

    Extended game_logic() example

--- src/game_solver_logic.py
+++ src/game_solver_logic.py
@@ -3,13 +3,16 @@ from typing import NamedTuple, Optional
 import pygame
 
 # rgba range: 0-255
-RGBA = NamedTuple('rgba', [('r', int), ('g', int), ('b', int), ('a', int)])
-MouseClickPos = NamedTuple('mouse_click_position', [('x', int), ('y', int)])
+RGBA = NamedTuple('RGBA', [('r', int), ('g', int), ('b', int), ('a', int)])
+MouseClickPos = NamedTuple('MouseClickPos', [('x', int), ('y', int)])
 
 
 # Todo: implement this function
 def game_logic(screen: pygame.Surface) -> Optional[MouseClickPos]:
     """This is where the game logic will be implemented."""
     p = RGBA(*screen.get_at((0, 0)))  # get pixel color at (0, 0)
+    height = screen.get_height()  # get screen height
+    width = screen.get_width()  # get screen width
     print(p.r, p.g, p.b, p.a)
+    print(height, width)
     return MouseClickPos(0, 0)
